ac942c8d8553733fd9ab39c341ac1f9612ef70b3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-03 17:02:33 -0400

    Pull out ListView into Stories component

---
+++ Stories.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Image,
+} from 'react-native';
+
+export default class Stories extends React.Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.state = {
+      stories: ds.cloneWithRows([]),
+    };
+  }
+
+  componentDidMount() {
+    this.queryFeed();
+  }
+
+  queryFeed() {
+    fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+      .then(response => response.json())
+      .then((responseJson) => {
+        const mostRead = responseJson.query.results.item;
+        console.log(mostRead[0])
+        this.setState({
+          stories: this.state.stories.cloneWithRows(mostRead),
+        });
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.stories}
+        renderRow={(rowData) => {
+          return (
+            <View>
+              <Image
+                styles={styles.thumbnails}
+                source={require('./images/placeholder.png')}
+              />
+              <Text>{rowData.title}</Text>
+              <Text>{rowData.description}</Text>
+            </View>
+          );
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+});
