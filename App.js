ece5d0e56e587f66d8a75ffda5db9e0b9cfa3c9d
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-03 15:44:03 -0400

    Query RSS feed with YQL to populate stories data
    https://developer.yahoo.com/yql/console/#h=select+*+from+rss+where+url%3D'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'

--- App.js
+++ App.js
@@ -16,6 +16,25 @@ export default class App extends React.Component {
       stories: ds.cloneWithRows([]),
     };
   }
+
+  componentDidMount() {
+    this.queryFeed();
+  }
+
+  queryFeed() {
+    fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+      .then(response => response.json())
+      .then((responseJson) => {
+        const mostRead = responseJson.query.results.item;
+        this.setState({
+          stories: this.state.stories.cloneWithRows(mostRead),
+        });
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
