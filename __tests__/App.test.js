06e2f2aa89abd367d2fc4abf8cf1f271860cbea3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-03 14:06:41 -0400

    Add test for render with Enzyme

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 
 import App from '../App';
 
@@ -8,4 +9,9 @@ describe('Main app', () => {
     const rendered = renderer.create(<App />);
     expect(rendered).toBeTruthy();
   });
+
+  test('render', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  })
 });
