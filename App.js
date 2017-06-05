fa4c6517686b648e7baea9c7732fb2e5f898bf05
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-05 09:44:53 -0400

    Add navigation to Main App

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  Dimensions,
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 import Stories from './Stories';
+
+const MainScreenNavigator = StackNavigator({
+  Home: {
+    screen: Stories,
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Stories />
+        <MainScreenNavigator style={styles.nav}/>
       </View>
     );
   }
@@ -22,5 +30,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  nav: {
+    width: Dimensions.get('window').width,
   },
 });
