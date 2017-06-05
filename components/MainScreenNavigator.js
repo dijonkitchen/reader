2117faa5a377df594c6472d2128c265b01a289c3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-05 12:26:20 -0400

    Add Story route
    ShortStory component is a placeholder for future Web View or Browser
    component

--- components/MainScreenNavigator.js
+++ components/MainScreenNavigator.js
@@ -1,9 +1,13 @@
 import { StackNavigator } from 'react-navigation';
 
 import Stories from './Stories';
+import ShortStory from './ShortStory';
 
 export default MainScreenNavigator = StackNavigator({
   Home: {
     screen: Stories,
+  },
+  Story: {
+    screen: ShortStory,
   },
 });
