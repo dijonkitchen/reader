c2776238203e323089f799be8934462bc0308dc3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-05 15:22:48 -0400

    Add snapshot testing

--- components/__tests__/ShortStory.test.js
+++ components/__tests__/ShortStory.test.js
@@ -5,6 +5,7 @@ import {
   Text,
   TouchableHighlight,
 } from 'react-native';
+import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
 
 import ShortStory from '../ShortStory';
@@ -28,5 +29,12 @@ describe('ShortStory component', () => {
 
   test('should be contain two Text components', () => {
     expect(wrapper.find(Text).length).toBe(2);
+  });
+
+  test('renders correctly', () => {
+    const tree = renderer.create(
+      <ShortStory navigation={() => 'test'} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
