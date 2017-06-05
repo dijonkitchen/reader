4d6a4038366ed7527da42aa8a25163a980687748
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-05 16:11:40 -0400

    Store feed into AsyncStorage for Offline use

--- components/Stories.js
+++ components/Stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   StyleSheet,
   ListView,
+  AsyncStorage,
 } from 'react-native';
 
 import ShortStory from './ShortStory';
@@ -21,19 +22,38 @@ export default class Stories extends React.Component {
     this.queryFeed();
   }
 
-  queryFeed() {
+  async queryFeed() {
+    try {
+      const stories = await AsyncStorage.getItem('stories');
+      if (stories !== null) {
+        this.setState({
+          stories: this.state.stories.cloneWithRows(JSON.parse(stories)),
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
     fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
       .then(response => response.json())
       .then((responseJson) => {
         const mostRead = responseJson.query.results.item;
-        // console.log(mostRead[0])
         this.setState({
           stories: this.state.stories.cloneWithRows(mostRead),
         });
+        this.storeFeed(mostRead);
       })
       .catch((error) => {
-        console.error(error)
+        console.error(error);
       });
+  }
+
+  async storeFeed(stories) {
+    try {
+      await AsyncStorage.setItem('stories', JSON.stringify(stories))
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   render() {
