1f6ed01ef7e05c7abcfe5f1a1c62140c156cdf4c
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-06-06 18:15:12 -0400

    Add tests for TextInput and ListView

--- components/__tests__/Stories.test.js
+++ components/__tests__/Stories.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { ListView, View } from 'react-native';
+import {
+  ListView,
+  View,
+  TextInput,
+} from 'react-native';
 import renderer from 'react-test-renderer';
 import { shallow, mount, render } from 'enzyme';
 
@@ -16,6 +20,14 @@ describe('Stories component', () => {
 
   test('should be a certain component', () => {
     expect(wrapper.type()).toEqual(View);
+  });
+
+  test('should contain only one ListView component', () => {
+    expect(wrapper.find(ListView).length).toBe(1);
+  });
+
+  test('should contain only one TextInput component', () => {
+    expect(wrapper.find(TextInput).length).toBe(1);
   });
 
   test('renders correctly', () => {
